806fdf09ca26cf5d0e48cd8c952604d73ecafc78
Yusra Fatima <162156870+Usrafatima@users.noreply.github.com>
2025-02-08 18:56:46 +0500

    Update page.tsx

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -5,7 +5,7 @@ import AddToCartButton from "@/app/component/AddToCartButton/AddToCartButton";
 import Counter from "@/app/component/counter";
 import { FaHeart, FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaStar, FaRegStar } from "react-icons/fa";
 import Link from "next/link";
-import { Metadata } from "next";
+
 
 type Review = {
   user: string;
